3c0198751045c1c1d74b1f07068205a848ccdad7
gregreust <greg.reust@gmail.com>
2022-10-04 11:46:55 -0500

    Created React app

---
+++ music-library/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <nav>
+        <h1>Music Library</h1>
+      </nav>
+      <SongList/>
+    </body>
+
+  );
+}
+
+export default App;
