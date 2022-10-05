8b9029e6130a844f00d2799a1ba2b2343bfcf456
gregreust <greg.reust@gmail.com>
2022-10-05 11:06:48 -0500

    Added put functionality to like button

---
+++ music-library/src/Components/Search.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Search = () => {
+    return ( 
+        
+        // form with dropdown for parameter and input to search for 
+
+     );
+}
+ 
+export default Search;
--- music-library/src/Components/SongRow.jsx
+++ music-library/src/Components/SongRow.jsx
@@ -1,6 +1,18 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-const SongRow = (song) => {
+const SongRow = ({song, getAllSongs}) => {
+
+    // async function getAllSongs(){
+    //     const response = await axios.get(GET_ALL_SONGS_URL);
+    //     setSongs(response.data);
+    // }
+
+    const handleLike = async () => {
+        let newSong = {...song, likes:song.likes+1};
+        await axios.put(`http://127.0.0.1:8000/music/${song.id}/`, newSong).then(()=>{getAllSongs()});
+    }
+
     return ( 
         <div class="row">
             <div>{song.title}</div>
@@ -9,7 +21,7 @@ const SongRow = (song) => {
             <div>{song.release_date}</div>
             <div>{song.genre}</div>
             <div>{song.likes}</div>
-            <button>Like</button>
+            <button onClick={handleLike}>Like</button>
             <button>Edit</button>
         </div>
      );
